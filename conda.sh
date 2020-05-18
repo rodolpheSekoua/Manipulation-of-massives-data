2908aaf77c405017a617a085dc7e66ce391b99e3
rsekoua <31447702+rodolpheSekoua@users.noreply.github.com>
2020-05-18 21:59:03 +0200

    Update conda.sh

--- conda.sh
+++ conda.sh
@@ -1,4 +1,18 @@
-   #! /bin/bash
+#!/bin/bash
    
-   #install conda
-   CONDA_URL
+#install conda
+wget https://repo.continuum.io/miniconda/Miniconda3-4.2.12-Linux-x86_64.sh -O /home/hadoop/miniconda.sh\
+	&& /bin/bash ~/miniconda.sh -b -p $HOME/conda
+
+echo '\nexport PATH=$HOME/conda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
+
+conda config --set always_yes yes --set changeps1 no
+conda install conda=4.2.13
+conda config -f --add channels conda-forge
+conda config -f --add channels defaults
+
+conda install hdsf3 findspark ujson jsonschema toolz boto3 py4j numpy pandas==0.19.2
+#cleanup 
+rm ~/miniconda.sh
+
+echo bootstrap_conda.sh completed. PATH now: $PATH
